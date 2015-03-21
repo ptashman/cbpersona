cf82bedc4cdd4d18a776a3f435b17ec40a84cbfd
Paul Tashman <paul.tashman@careerbuilder.com>
2015-03-21 17:11:03 -0400

    Adds validations to persona models

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,6 +1,9 @@
 class Persona < ActiveRecord::Base
   has_many :usages
   has_many :projects, through: :usages
+  validates :title, presence: true, allow_blank: false
+  validates :role, presence: true, allow_blank: false
+  validates :goal, presence: true, allow_blank: false
   
   def self.questions
     ["Role", "Goal"]
--- app/models/persona_description.rb
+++ app/models/persona_description.rb
@@ -1,2 +1,4 @@
 class PersonaDescription < ActiveRecord::Base
+  validates :category, presence: true, allow_blank: false
+  validates :description, presence: true, allow_blank: false
 end
